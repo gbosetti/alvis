1dfe004494cd9c88c22c0f425ddd4139e379fa1d
SantiMaceri <santimaceri@gmail.com>
2018-08-13 12:16:37 -0300

    StandardTable updated, almost ready

--- StandardTableStrategy.js
+++ StandardTableStrategy.js
@@ -1,8 +1,9 @@
 
 
 function convertDataFromTable() {  // La tabla deberia llegar por parametro, en este caso se usa directamente del dom
-    'use strict';					// Podemos usar jquery o no, hay que decidir
-    var json = {};
+    'use strict';
+    var array = [];					// Podemos usar jquery o no, hay que decidir
+    
     var tabla = document.getElementById('tablaejemplo');
     var theads = tabla.querySelectorAll('th');
    
@@ -10,14 +11,15 @@ function convertDataFromTable() {  // La tabla deberia llegar por parametro, en 
    
   	for (var i= 1 ; i < tabla.querySelectorAll('tr').length ; i++) {
     	var row = (tabla.querySelectorAll('tr')[i]);
-    	
+    	var json = {};
     	for ( var j = 0 ; j < row.querySelectorAll('td').length ; j ++ ){
     		
     		json[theads[j].innerText] = row.querySelectorAll('td')[j].innerText;
     	}
+    	array.push(json);
     } 
 
-    console.log(json);
+     console.log(array);
 }
 
 
