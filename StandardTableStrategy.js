70ffb1638d0b16e11d7e2e7442f68d8999716128
SantiMaceri <santimaceri@gmail.com>
2018-08-13 12:09:24 -0300

    StandardTable update, not working

--- StandardTableStrategy.js
+++ StandardTableStrategy.js
@@ -2,8 +2,23 @@
 
 function convertDataFromTable() {  // La tabla deberia llegar por parametro, en este caso se usa directamente del dom
     'use strict';					// Podemos usar jquery o no, hay que decidir
-
+    var json = {};
     var tabla = document.getElementById('tablaejemplo');
     var theads = tabla.querySelectorAll('th');
-    console.log(theads[0].innerText);
+   
+
+   
+  	for (var i= 1 ; i < tabla.querySelectorAll('tr').length ; i++) {
+    	var row = (tabla.querySelectorAll('tr')[i]);
+    	
+    	for ( var j = 0 ; j < row.querySelectorAll('td').length ; j ++ ){
+    		
+    		json[theads[j].innerText] = row.querySelectorAll('td')[j].innerText;
+    	}
+    } 
+
+    console.log(json);
 }
+
+
+
