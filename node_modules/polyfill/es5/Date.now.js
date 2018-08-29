be35e8df1542a1347c05ced3c6edab6cd3625c05
Feli Pisoni <feli.pisoni@gmail.com>
2018-08-29 18:50:57 -0300

    Deleting node_modules (gitignore error)

--- node_modules/polyfill/es5/Date.now.js
+++
@@ -1,6 +0,0 @@
-// From https://developer.mozilla.org/en-US/docs/JavaScript/Reference/Global_Objects/Date/now
-if (!Date.now) {
-    Date.now = function now() {
-        return +new Date;
-    };
-}
--- node_modules/polyfill/es5/String.prototype.trim.js
+++
@@ -1,6 +0,0 @@
-// from https://developer.mozilla.org/en-US/docs/JavaScript/Reference/Global_Objects/String/trim
-if (!String.prototype.trim) {
-    String.prototype.trim = function () {
-        return this.replace(/^\s+|\s+$/g,'');
-    };
-}
