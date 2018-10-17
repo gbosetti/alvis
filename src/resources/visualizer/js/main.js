a0fac92a2ac74d8a18cc1fb688614a8208ee9be2
ulises-jeremias <ulisescf.24@gmail.com>
2018-10-17 10:02:32 -0300

    Fix issue https://gitlabsarha.lifia.info.unlp.edu.ar/SARHA/code/issues/72

--- src/resources/visualizer/js/main.js
+++ src/resources/visualizer/js/main.js
@@ -2,8 +2,9 @@ const infovis = new Infovis();
 
 browser.runtime.sendMessage({call: "notifyDocumentLoaded"})
   .then(dataset => {
-    if (dataset) {
-      return infovis.presentData(dataset);
+    if (dataset && dataset.currentDataset) {
+      console.table(dataset.currentDataset);
+      return infovis.presentData(dataset.currentDataset);
     }
     
     throw new Error("No dataset");
