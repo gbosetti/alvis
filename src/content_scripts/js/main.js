7b865cf8c397b5fa16df5f237b32cb1484ad605c
feli.pisoni@gmail.com <feli.pisoni@gmail.com>
2018-10-24 18:45:00 -0300

    Advancing NoHeaderListStrategy, implemented Visitor pattern

---
+++ src/content_scripts/js/main.js
@@ -0,0 +1,9 @@
+const tableManager = new TableManager(); 
+
+browser.runtime.onMessage.addListener(function callPageSideActions(request, sender) {
+  console.log(`Message: ${request.message} in TableManager.`);
+  if (tableManager[request.message]) {
+    return tableManager[request.message](request.args); 
+  } 
+  throw new Error("Wrong message.");
+}).catch(console.log);
